758688bb91523f94dbf24b3a64884a3fc233f8d1
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-03-04 23:32:44 +0100

    test

---
+++ keymaps/sina/rules.mk
@@ -0,0 +1,11 @@
+# ...existing code...
+
+# No persistent EEPROM backend
+EEPROM_DRIVER = transient
+
+# Disable features that require persistent keymap/macro storage
+DYNAMIC_KEYMAP_ENABLE = no
+VIA_ENABLE = no
+VIAL_ENABLE = no
+
+# ...existing code...
