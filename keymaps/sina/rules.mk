5332a07643aa0a78d72d6a6150b221a084484c22
Sina <sinagessner.wuppertal@gmail.com>
2026-03-06 00:07:08 +0100

    Enable VIA and dynamic keymap support

--- keymaps/sina/rules.mk
+++ keymaps/sina/rules.mk
@@ -1,13 +1,7 @@
-# Runnable-by-default profile.
-# Set VIA_ENABLE=yes in your build command/environment to enable VIA.
-VIA_ENABLE = no
-VIAL_ENABLE = no
-DYNAMIC_KEYMAP_ENABLE = no
-
-# VIA requires persistent storage for dynamic keymaps/macros.
-ifeq ($(VIA_ENABLE),yes)
+# EEPROM backend (needed for VIA + dynamic keymap)
 EEPROM_DRIVER = wear_leveling
-WEAR_LEVELING_DRIVER = embedded_flash
-VIAL_ENABLE = no
+
+# Enable VIA (and required dynamic keymap), disable Vial
 DYNAMIC_KEYMAP_ENABLE = yes
-endif
+VIA_ENABLE = yes
+VIAL_ENABLE = no
